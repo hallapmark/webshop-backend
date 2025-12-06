8625239e4b19078cedc06c02492187e448919286
Mark Hallap <hallapmark@gmail.com>
2025-12-06 17:33:03 +0200

    Add ability to find person by email to repository

--- src/main/java/ee/markh/webshopbackend/repository/PersonRepository.java
+++ src/main/java/ee/markh/webshopbackend/repository/PersonRepository.java
@@ -3,6 +3,9 @@ package ee.markh.webshopbackend.repository;
 import ee.markh.webshopbackend.entity.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PersonRepository extends JpaRepository<Person, Long> {
 
+    Person findByEmail(String email);
 }
